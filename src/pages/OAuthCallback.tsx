d6380e2f9217bc74740ea70708a428c315e1571b
zuu3 <ojh27467242@gmail.com>
2025-05-13 12:23:33 +0900

    feat(#1) :: 로그인 요청 변경

--- src/pages/OAuthCallback.tsx
+++ src/pages/OAuthCallback.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import customAxios from '../lib/customAxios';
 import { useAuthStore } from '../store/authStore';
 
 export default function OAuthCallback() {
+  const [searchParams] = useSearchParams();
   const setUser = useAuthStore((s) => s.setUser);
+  const code = searchParams.get('code');
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await customAxios.get('/users/me');
+        await customAxios.get(`/oauth2/login/google/${code}`);
+        const res = await customAxios.get('/authority/check');
         setUser(res.data);
         navigate('/dm');
       } catch (e) {
